e37270a64f8ca2d0b1620ba8ba2940f03abeabb1
CD777XC <96194378+CD777XC@users.noreply.github.com>
2022-04-15 11:15:00 +0200

    Uploaded submit 3.0
    HTML Form w/ JS file on development

---
+++ submit.js
@@ -0,0 +1,21 @@
+// Submit form functions 
+
+// const userName = document.querySelector('#username')
+// const emailEl = document.querySelector('#email')
+// const messageEl = document.querySelector('#message')
+// const buttonEl = document.querySelector('#button')
+
+let formSubmission = [{
+    user: '',
+    email:'', 
+    message: '', 
+}]
+
+// Create the send(f) function to send values from the form to the forSubmission object
+function send(){
+    formSubmission[0].user.push() = 'LowGok'
+    console.log(formSubmission)
+}
+
+console.log(send())
+
