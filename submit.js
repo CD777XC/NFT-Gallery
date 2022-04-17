d8ad98193c9a113bd728a0abb248dbb0282fd8d2
CD777XC <96194378+CD777XC@users.noreply.github.com>
2022-04-17 14:24:26 +0200

    Add files updating
    Front page and code optimization

--- submit.js
+++ submit.js
@@ -1,21 +1,21 @@
 // Submit form functions 
 
-// const userName = document.querySelector('#username')
-// const emailEl = document.querySelector('#email')
-// const messageEl = document.querySelector('#message')
-// const buttonEl = document.querySelector('#button')
+const formEl = document.querySelector('.form-group')
+const userName = document.querySelector('#username')
+const emailEl = document.querySelector('#email')
+const messageEl = document.querySelector('#message')
+const buttonEl = document.querySelector('#button')
+const statusMsg = document.querySelector('#status')
 
-let formSubmission = [{
-    user: '',
-    email:'', 
-    message: '', 
-}]
-
-// Create the send(f) function to send values from the form to the forSubmission object
-function send(){
-    formSubmission[0].user.push() = 'LowGok'
-    console.log(formSubmission)
+let formSubmission = {
+    user: userName.value,
+    email: emailEl.value, 
+    message: messageEl.value, 
 }
 
-console.log(send())
+// Create the send(f) function to send values from the form to the forSubmission object 
+// API to be learnt as well as MySQL 
+// Process on hold - Flip NFT on progress
 
+
+// Want to display the error or success message right here
